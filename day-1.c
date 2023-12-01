fb54cd5d412ea731c09def3d7de26ef2de09bade
Mister Hat <misterhat144@gmail.com>
2023-12-01 16:14:05 -0600

    fclose and minor refactor

--- day-1.c
+++ day-1.c
@@ -10,16 +10,16 @@
 const char *number_names[] = {"one", "two",   "three", "four", "five",
                               "six", "seven", "eight", "nine"};
 
-/* grab the first and last digit (either a digit or a number name) and find
- * the sum */
-uint32_t get_line_sum(const char *line) {
+/* grab the first and last digit (either a digit or a number name) and turn
+ * into an integer */
+uint32_t get_line_int(const char *line) {
     int first_digit_index = -1;
     int last_digit_index = -1;
 
     int first_name_index = -1;
     int last_name_index = -1;
 
-    /* initially set with the number name ints, but overriden if digits
+    /* initially set with the number name ints, but overriden if digit
      * precedes or follows them respectively */
     uint32_t first_int = 0;
     uint32_t last_int = 0;
@@ -83,12 +83,14 @@ int main(int argc, char **argv) {
     char *file_buffer = calloc(input_size, sizeof(char));
     fread(file_buffer, sizeof(char), input_size, input_file);
 
+    fclose(input_file);
+
     char *line = strtok(file_buffer, "\n");
 
     uint32_t total = 0;
 
     while (line != NULL) {
-        total += get_line_sum(line);
+        total += get_line_int(line);
         line = strtok(NULL, "\n");
     }
 
