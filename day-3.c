2732fb6a045c5a2b554da886bf79f60caa802e12
Mister Hat <misterhat144@gmail.com>
2023-12-03 20:55:41 -0600

    day 3 part 2

--- day-3.c
+++ day-3.c
@@ -1,47 +1,66 @@
 #include <ctype.h>
-#include <math.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define PART_TWO true
+
+typedef struct Gear {
+    size_t index;
+    uint32_t parts[2];
+} Gear;
+
 typedef struct Grid {
     uint32_t columns;
     uint32_t rows;
     char *grid;
+
+    size_t gears_length;
+    Gear *gears;
 } Grid;
 
 void grid_init(Grid *grid) {
     grid->grid = calloc(grid->columns * grid->rows, sizeof(char));
+    grid->gears = calloc((grid->columns * grid->rows) / 2, sizeof(Gear));
 }
 
-void grid_destroy(Grid *grid) { free(grid->grid); }
+void grid_destroy(Grid *grid) {
+    free(grid->grid);
+    free(grid->gears);
+}
 
 size_t grid_get_index(Grid *grid, uint32_t row, uint32_t column) {
     return row * grid->columns + column;
 }
 
-char grid_get_char(Grid *grid, uint32_t row, uint32_t column) {
-    return grid->grid[grid_get_index(grid, row, column)];
-}
-
 bool is_symbol(char c) { return c != '.' && !isdigit(c); }
 
 /* check if digits are adjacent to any non-period symbols in grid */
-bool is_symbol_adjacent(Grid *grid, size_t digits_length, uint32_t digits_row,
-                        uint32_t digits_column) {
+char grid_symbol_adjacent(Grid *grid, size_t digits_length, uint32_t digits_row,
+                          uint32_t digits_column, int *symbol_index) {
     /* check to the left of the digits */
-    if (digits_column > 0 &&
-        is_symbol(grid_get_char(grid, digits_row, digits_column - 1))) {
-        return true;
+    if (digits_column > 0) {
+        *symbol_index = grid_get_index(grid, digits_row, digits_column - 1);
+
+        char c = grid->grid[*symbol_index];
+
+        if (is_symbol(c)) {
+            return c;
+        }
     }
 
     /* check to the right of the digits */
-    if (digits_column + digits_length < grid->columns &&
-        is_symbol(
-            grid_get_char(grid, digits_row, digits_column + digits_length))) {
-        return true;
+    if (digits_column + digits_length < grid->columns) {
+        *symbol_index =
+            grid_get_index(grid, digits_row, digits_column + digits_length);
+
+        char c = grid->grid[*symbol_index];
+
+        if (is_symbol(c)) {
+            return c;
+        }
     }
 
     int start_column = digits_column - 1;
@@ -59,8 +78,12 @@ bool is_symbol_adjacent(Grid *grid, size_t digits_length, uint32_t digits_row,
     /* check the row above digits */
     if (digits_row > 0) {
         for (int column = start_column; column < end_column; column++) {
-            if (is_symbol(grid_get_char(grid, digits_row - 1, column))) {
-                return true;
+            *symbol_index = grid_get_index(grid, digits_row - 1, column);
+
+            char c = grid->grid[*symbol_index];
+
+            if (is_symbol(c)) {
+                return c;
             }
         }
     }
@@ -68,13 +91,32 @@ bool is_symbol_adjacent(Grid *grid, size_t digits_length, uint32_t digits_row,
     /* check the row below digits */
     if (digits_row < grid->rows - 1) {
         for (int column = start_column; column < end_column; column++) {
-            if (is_symbol(grid_get_char(grid, digits_row + 1, column))) {
-                return true;
+            *symbol_index = grid_get_index(grid, digits_row + 1, column);
+
+            char c = grid->grid[*symbol_index];
+
+            if (is_symbol(c)) {
+                return c;
             }
         }
     }
 
-    return false;
+    return '\0';
+}
+
+void grid_gear_add(Grid *grid, size_t index, uint32_t part) {
+    for (int i = 0; i < grid->gears_length; i++) {
+        Gear *gear = grid->gears + i;
+
+        if (grid->gears[i].index == index) {
+            gear->parts[1] = part;
+            return;
+        }
+    }
+
+    Gear *gear = (grid->gears + grid->gears_length++);
+    gear->index = index;
+    gear->parts[0] = part;
 }
 
 int main(int argc, char **argv) {
@@ -123,6 +165,7 @@ int main(int argc, char **argv) {
             if (isdigit(grid.grid[grid_index])) {
                 digits_length++;
 
+                /* handle end of line digits */
                 if (column == grid.columns - 1) {
                     grid_index++;
                     column++;
@@ -143,11 +186,22 @@ int main(int argc, char **argv) {
 
                 digit_str[digits_length] = '\0';
 
-                // printf("%s\n", digit_str);
+                int symbol_index = -1;
 
-                if (is_symbol_adjacent(&grid, digits_length, row,
-                                       column - digits_length)) {
-                    sum += atoi(digit_str);
+                char c =
+                    grid_symbol_adjacent(&grid, digits_length, row,
+                                         column - digits_length, &symbol_index);
+
+                if (c != '\0') {
+                    uint32_t part = atoi(digit_str);
+
+                    if (c == '*') {
+                        grid_gear_add(&grid, symbol_index, part);
+                    }
+
+                    if (!PART_TWO) {
+                        sum += part;
+                    }
                 }
 
                 digits_length = 0;
@@ -155,7 +209,16 @@ int main(int argc, char **argv) {
         }
     }
 
-    // printf("%d\n", is_symbol_adjacent(&grid, 2, 0, 2));
+    if (PART_TWO) {
+        for (int i = 0; i < grid.gears_length; i++) {
+            Gear gear = grid.gears[i];
+
+            if (gear.parts[1] != 0) {
+                sum += gear.parts[0] * gear.parts[1];
+            }
+        }
+    }
+
     printf("%d\n", sum);
 
     grid_destroy(&grid);
